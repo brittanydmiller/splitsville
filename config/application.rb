da6b107b3009ecc5608dc1b9c433bd8ccb96f3b2
Brittany Miller <brittanydmiller@gmail.com>
2018-02-18 17:04:19 -0800

    Automatically establish AR<=>DB connection

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Splitsville
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    console do
+      ActiveRecord::Base.connection
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
